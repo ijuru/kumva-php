cdce68f82a420a724ef9a1d8aa04d0198a738e0f
Rowan Seymour <rowanseymour@gmail.com>
2011-04-13 21:54:04 +0200

    Removed temp update scripts

--- install/3update.sql
+++
@@ -1,9 +0,0 @@
-
-# Enforce uniqueness of definition.change_id
-ALTER TABLE `rw_definition` ADD UNIQUE INDEX `UQ_rw_definition_change` (`change_id` ASC);
-
-# Make definition.entry_id not null
-ALTER TABLE `kumva`.`rw_definition` CHANGE COLUMN `entry_id` `entry_id` INT(10) UNSIGNED NOT NULL;
-
-# Make definition.revision not null	
-ALTER TABLE `kumva`.`rw_definition` CHANGE COLUMN `revision` `revision` INT(10) UNSIGNED NOT NULL;
