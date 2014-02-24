bbc4a3ff8da6468af0c823713a56219e66f9b94a
Rowan Seymour <rowanseymour@gmail.com>
2014-02-24 13:32:48 +0200

    Fixed typos on possessives reference page

--- themes/kinyarwanda/pages/possessives.php
+++ themes/kinyarwanda/pages/possessives.php
@@ -20,7 +20,7 @@
  * Purpose: Adjectives reference page
  */
 ?>    
-<p>Possessives modify a noun to describe it's ownership by, or relationship to, another noun.</p>
+<p>Possessives modify a noun to describe its ownership by, or relationship to, another noun.</p>
 
 <a name="participles"></a>	
 <h3>Possessive participles</h3>
@@ -205,11 +205,11 @@
 
 <p>The above stems are only used when describing ownership by a person or people (i.e. a class 1 or 2 noun), but of course a noun of any class can own a noun of any other class. For the remaining classes, the stem is created from the possessive participle by replacing the <span lang="rw">a</span> with <span lang="rw">o</span>, For example, if we are referring to something owned by a cow, then the stem is <span lang="rw">-yo:</span></p>
 <ul>
-	<li><span lang="rw">umutwe wayo</span> - it's head</li>
-	<li><span lang="rw">ikirengi cyayo</span> - it's foot</li>
-	<li><span lang="rw">amahembe yayo</span> - it's horns</li>
-	<li><span lang="rw">ubwoya bwayo</span> - it's hair</li>
-	<li><span lang="rw">ukuboko kwayo</span> - it's front leg (lit. arm)</li>
+	<li><span lang="rw">umutwe wayo</span> - its head</li>
+	<li><span lang="rw">ikirengi cyayo</span> - its foot</li>
+	<li><span lang="rw">amahembe yayo</span> - its horns</li>
+	<li><span lang="rw">ubwoya bwayo</span> - its hair</li>
+	<li><span lang="rw">ukuboko kwayo</span> - its front leg (lit. arm)</li>
 </ul>
 
 <p>This does mean there are an intimidatingly large number of possessive adjectives, so one shouldn't try to memorize them all, but rather understand how they are formed. The stems for all of the remaining classes are given below:</p>
@@ -227,112 +227,112 @@
 		<td>3</td>
 		<td><span lang="rw"><?php ku_query('umu*', 'umu-'); ?></span>, <span lang="rw"><?php ku_query('umw*', 'umw-'); ?></span></td>
 		<td><span lang="rw">-wo</span></td>
-		<td><span lang="rw">umwana wawo</span> - it's child</td>
-		<td><span lang="rw">igitabo cyawo</span> - it's book</td>
-		<td><span lang="rw">inka yawo</span> - it's cow</td>
+		<td><span lang="rw">umwana wawo</span> - its child</td>
+		<td><span lang="rw">igitabo cyawo</span> - its book</td>
+		<td><span lang="rw">inka yawo</span> - its cow</td>
 	</tr>
 	<tr>
 		<td>4</td>
 		<td><span lang="rw"><?php ku_query('imi*', 'imi-'); ?></span>, <span lang="rw"><?php ku_query('imy*', 'imy-'); ?></span></td>
 		<td><span lang="rw">-yo</span></td>
-		<td><span lang="rw">umwana wayo</span> - it's child</td>
-		<td><span lang="rw">igitabo cyayo</span> - it's book</td>
-		<td><span lang="rw">inka yayo</span> - it's cow</td>
+		<td><span lang="rw">umwana wayo</span> - its child</td>
+		<td><span lang="rw">igitabo cyayo</span> - its book</td>
+		<td><span lang="rw">inka yayo</span> - its cow</td>
 	</tr>
 	<tr>
 		<td>5</td>
 		<td><span lang="rw"><?php ku_query('i*', 'i-'); ?></span>, <span lang="rw"><?php ku_query('iri*', 'iri-'); ?></span></td>
 		<td><span lang="rw">-ryo</span></td>
-		<td><span lang="rw">umwana waryo</span> - it's child</td>
-		<td><span lang="rw">igitabo cyaryo</span> - it's book</td>
-		<td><span lang="rw">inka yaryo</span> - it's cow</td>
+		<td><span lang="rw">umwana waryo</span> - its child</td>
+		<td><span lang="rw">igitabo cyaryo</span> - its book</td>
+		<td><span lang="rw">inka yaryo</span> - its cow</td>
 	</tr>
 	<tr>
 		<td>6</td>
 		<td><span lang="rw"><?php ku_query('ama*', 'ama-'); ?></span>, <span lang="rw"><?php ku_query('am*', 'am-'); ?></span></td>
 		<td><span lang="rw">-yo</span></td>
-		<td><span lang="rw">umwana wayo</span> - it's child</td>
-		<td><span lang="rw">igitabo cyayo</span> - it's book</td>
-		<td><span lang="rw">inka yayo</span> - it's cow</td>
+		<td><span lang="rw">umwana wayo</span> - its child</td>
+		<td><span lang="rw">igitabo cyayo</span> - its book</td>
+		<td><span lang="rw">inka yayo</span> - its cow</td>
 	</tr>
 	<tr>
 		<td>7</td>
 		<td><span lang="rw"><?php ku_query('iki*', 'iki-'); ?></span>, <span lang="rw"><?php ku_query('icy*', 'icy-'); ?></span>, <span lang="rw"><?php ku_query('igi*', 'igi-'); ?></span></td>
 		<td><span lang="rw">-cyo</span></td>
-		<td><span lang="rw">umwana wacyo</span> - it's child</td>
-		<td><span lang="rw">igitabo cyacyo</span> - it's book</td>
-		<td><span lang="rw">inka yacyo</span> - it's cow</td>
+		<td><span lang="rw">umwana wacyo</span> - its child</td>
+		<td><span lang="rw">igitabo cyacyo</span> - its book</td>
+		<td><span lang="rw">inka yacyo</span> - its cow</td>
 	</tr>
 	<tr>
 		<td>8</td>
 		<td><span lang="rw"><?php ku_query('ibi*', 'ibi-'); ?></span>, <span lang="rw"><?php ku_query('iby*', 'ibi-'); ?></span></td>
 		<td><span lang="rw">-byo</span></td>
-		<td><span lang="rw">umwana wabyo</span> - it's child</td>
-		<td><span lang="rw">igitabo cyabyo</span> - it's book</td>
-		<td><span lang="rw">inka yabyo</span> - it's cow</td>
+		<td><span lang="rw">umwana wabyo</span> - its child</td>
+		<td><span lang="rw">igitabo cyabyo</span> - its book</td>
+		<td><span lang="rw">inka yabyo</span> - its cow</td>
 	</tr>
 	<tr>
 		<td>9</td>
 		<td rowspan="2"><span lang="rw"><?php ku_query('i*', 'i-'); ?></span>, <span lang="rw"><?php ku_query('in*', 'in-'); ?></span>, <span lang="rw"><?php ku_query('inz*', 'inz-'); ?></span></td>
 		<td><span lang="rw">-yo</span></td>
-		<td><span lang="rw">umwana wayo</span> - it's child</td>
-		<td><span lang="rw">igitabo cyayo</span> - it's book</td>
-		<td><span lang="rw">inka yayo</span> - it's cow</td>
+		<td><span lang="rw">umwana wayo</span> - its child</td>
+		<td><span lang="rw">igitabo cyayo</span> - its book</td>
+		<td><span lang="rw">inka yayo</span> - its cow</td>
 	</tr>
 	<tr>
 		<td>10</td>
 		<td><span lang="rw">-zo</span></td>
-		<td><span lang="rw">umwana wazo</span> - it's child</td>
-		<td><span lang="rw">igitabo cyazo</span> - it's book</td>
-		<td><span lang="rw">inka yazo</span> - it's cow</td>
+		<td><span lang="rw">umwana wazo</span> - its child</td>
+		<td><span lang="rw">igitabo cyazo</span> - its book</td>
+		<td><span lang="rw">inka yazo</span> - its cow</td>
 	</tr>
 	<tr>
 		<td>11</td>
 		<td><span lang="rw"><?php ku_query('uru*', 'uru-'); ?></span>, <span lang="rw"><?php ku_query('urw*', 'urw-'); ?></span></td>
 		<td><span lang="rw">-rwo</span></td>
-		<td><span lang="rw">umwana warwo</span> - it's child</td>
-		<td><span lang="rw">igitabo cyarwo</span> - it's book</td>
-		<td><span lang="rw">inka yarwo</span> - it's cow</td>
+		<td><span lang="rw">umwana warwo</span> - its child</td>
+		<td><span lang="rw">igitabo cyarwo</span> - its book</td>
+		<td><span lang="rw">inka yarwo</span> - its cow</td>
 	</tr>
 	<tr>
 		<td>12</td>
 		<td><span lang="rw"><?php ku_query('aka*', 'aka-'); ?></span>, <span lang="rw"><?php ku_query('ak*', 'ak-'); ?></span>, <span lang="rw"><?php ku_query('aga*', 'aga-'); ?></span></td>
 		<td><span lang="rw">-ko</span></td>
-		<td><span lang="rw">umwana wako</span> - it's child</td>
-		<td><span lang="rw">igitabo cyako</span> - it's book</td>
-		<td><span lang="rw">inka yako</span> - it's cow</td>
+		<td><span lang="rw">umwana wako</span> - its child</td>
+		<td><span lang="rw">igitabo cyako</span> - its book</td>
+		<td><span lang="rw">inka yako</span> - its cow</td>
 	</tr>
 	<tr>
 		<td>13</td>
 		<td><span lang="rw"><?php ku_query('utu*', 'utu-'); ?></span>, <span lang="rw"><?php ku_query('utw*', 'utw-'); ?></span>, <span lang="rw"><?php ku_query('udu*', 'udu-'); ?></span></td>
 		<td><span lang="rw">-two</span></td>
-		<td><span lang="rw">umwana watwo</span> - it's child</td>
-		<td><span lang="rw">igitabo cyatwo</span> - it's book</td>
-		<td><span lang="rw">inka yatwo</span> - it's cow</td>
+		<td><span lang="rw">umwana watwo</span> - its child</td>
+		<td><span lang="rw">igitabo cyatwo</span> - its book</td>
+		<td><span lang="rw">inka yatwo</span> - its cow</td>
 	</tr>
 	<tr>
 		<td>14</td>
 		<td><span lang="rw"><?php ku_query('ubu*', 'ubu-'); ?></span>, <span lang="rw"><?php ku_query('ubw*', 'ubw-'); ?></span></td>
 		<td><span lang="rw">-bwo</span></td>
-		<td><span lang="rw">umwana wabwo</span> - it's child</td>
-		<td><span lang="rw">igitabo cyabwo</span> - it's book</td>
-		<td><span lang="rw">inka yabwo</span> - it's cow</td>
+		<td><span lang="rw">umwana wabwo</span> - its child</td>
+		<td><span lang="rw">igitabo cyabwo</span> - its book</td>
+		<td><span lang="rw">inka yabwo</span> - its cow</td>
 	</tr>
 	<tr>
 		<td>15</td>
 		<td><span lang="rw"><?php ku_query('uku*', 'uku-'); ?></span>, <span lang="rw"><?php ku_query('ukw*', 'ukw-'); ?></span>, <span lang="rw"><?php ku_query('ugu*', 'ugu-'); ?></span></td>
 		<td><span lang="rw">-kwo</span></td>
-		<td><span lang="rw">umwana wakwo</span> - it's child</td>
-		<td><span lang="rw">igitabo cyakwo</span> - it's book</td>
-		<td><span lang="rw">inka yakwo</span> - it's cow</td>
+		<td><span lang="rw">umwana wakwo</span> - its child</td>
+		<td><span lang="rw">igitabo cyakwo</span> - its book</td>
+		<td><span lang="rw">inka yakwo</span> - its cow</td>
 	</tr>
 	<tr>
 		<td>16</td>
 		<td><span lang="rw"><?php ku_query('aha*', 'aha-'); ?></span>, <span lang="rw"><?php ku_query('ah*', 'ah-'); ?></span></td>
 		<td><span lang="rw">-ho</span></td>
-		<td><span lang="rw">umwana waho</span> - it's child</td>
-		<td><span lang="rw">igitabo cyaho</span> - it's book</td>
-		<td><span lang="rw">inka yaho</span> - it's cow</td>
+		<td><span lang="rw">umwana waho</span> - its child</td>
+		<td><span lang="rw">igitabo cyaho</span> - its book</td>
+		<td><span lang="rw">inka yaho</span> - its cow</td>
 	</tr>
 </table>
 
